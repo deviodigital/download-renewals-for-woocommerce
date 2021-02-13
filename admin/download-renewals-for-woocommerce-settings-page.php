e59f2d73851094fdae1a03ae25ef7ee723c08794
Robert DeVore <deviodigital@gmail.com>
2021-02-12 21:21:10 -0500

    :construction: WIP: Added settings page (blank_

---
+++ admin/download-renewals-for-woocommerce-settings-page.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * The file responsible for creating the DRWC Settings page
+ *
+ * @link       https://www.deviodigital.com
+ * @since      1.0.0
+ *
+ * @package    Download_Renewals_For_Woocommerce
+ * @subpackage Download_Renewals_For_Woocommerce/admin
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
