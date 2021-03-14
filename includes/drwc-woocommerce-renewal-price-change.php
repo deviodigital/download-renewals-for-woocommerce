55a1e99eff400426238a55618d8db6cca74a7590
Robert DeVore <deviodigital@gmail.com>
2021-03-14 11:04:41 -0400

    :construction: WIP: Update product price to check sitecare_user_has_bought_items before updating price

--- includes/drwc-woocommerce-renewal-price-change.php
+++ includes/drwc-woocommerce-renewal-price-change.php
@@ -20,7 +20,7 @@ if ( ! defined( 'WPINC' ) ) {
  * @since  1.0
  * @return string $price_html
  */ 
-function drwc_renewal_price_display( $price, $product ) {	
+function drwc_renewal_price_display( $price, $product ) {
     global $post;
 
     // Post ID.
@@ -49,24 +49,24 @@ add_filter( 'woocommerce_get_price', 'drwc_renewal_price_display', 10, 2 );
  */
 function sitecare_user_has_bought_items( $user_var = 0,  $product_ids = 0 ) {
     global $wpdb;
-    
-    // Based on user ID (registered users)
-    if ( is_numeric( $user_var) ) { 
-        $meta_key     = '_customer_user';
-        $meta_value   = $user_var == 0 ? (int) get_current_user_id() : (int) $user_var;
-    } 
-    // Based on billing email (Guest users)
+
+    // Based on user ID (registered users).
+    if ( is_numeric( $user_var ) ) {
+        $meta_key   = '_customer_user';
+        $meta_value = $user_var == 0 ? (int) get_current_user_id() : (int) $user_var;
+    }
+
+    // Based on billing email (Guest users).
     else { 
-        $meta_key     = '_billing_email';
-        $meta_value   = sanitize_email( $user_var );
+        $meta_key   = '_billing_email';
+        $meta_value = sanitize_email( $user_var );
     }
     
     $paid_statuses    = array_map( 'esc_sql', wc_get_is_paid_statuses() );
     $product_ids      = is_array( $product_ids ) ? implode(',', $product_ids) : $product_ids;
-
     $line_meta_value  = $product_ids !=  ( 0 || '' ) ? 'AND woim.meta_value IN ('.$product_ids.')' : 'AND woim.meta_value != 0';
 
-    // Count the number of products
+    // Count the number of products.
     $count = $wpdb->get_var( "
         SELECT COUNT(p.ID) FROM {$wpdb->prefix}posts AS p
         INNER JOIN {$wpdb->prefix}postmeta AS pm ON p.ID = pm.post_id
@@ -78,7 +78,7 @@ function sitecare_user_has_bought_items( $user_var = 0,  $product_ids = 0 ) {
         AND woim.meta_key IN ( '_product_id', '_variation_id' ) $line_meta_value 
     " );
 
-    // Return true if count is higher than 0 (or false)
+    // Return true if count is higher than 0 (or false).
     return $count > 0 ? true : false;
 }
 
@@ -87,20 +87,25 @@ function sitecare_user_has_bought_items( $user_var = 0,  $product_ids = 0 ) {
  * 
  * @since  1.0
  * @return void
- */ 
+ */
 function drwc_update_renewal_price_in_cart( $cart ) {
- 
+
     // This is necessary for WC 3.0+
-    if ( is_admin() && ! defined( 'DOING_AJAX' ) )
+    if ( is_admin() && ! defined( 'DOING_AJAX' ) ) {
         return;
+    }
 
     // Avoiding hook repetition (when using price calculations for example).
-    if ( did_action( 'woocommerce_before_calculate_totals' ) >= 2 )
+    if ( did_action( 'woocommerce_before_calculate_totals' ) >= 2 ) {
         return;
+    }
+
+    // Get user ID.
+    $user_id = get_current_user_id();
 
     // Loop through cart items.
     foreach ( $cart->get_cart() as $item ) {
-        if ( get_post_meta( $item['product_id'], 'drwc_renewal_price', true ) ) {
+        if ( get_post_meta( $item['product_id'], 'drwc_renewal_price', true ) && sitecare_user_has_bought_items( $user_id, $item['product_id'] ) ) {
             $item['data']->set_price( get_post_meta( $item['product_id'], 'drwc_renewal_price', true ) );
         }
     }
